36a0f9e867a2ca28908c945504414622ee72664a
Yoshani <yoshani.malinka@gmail.com>
2022-03-14 16:40:28 +0530

    login/signup page update

--- lib/screens/auth/login.dart
+++ lib/screens/auth/login.dart
@@ -1,5 +1,4 @@
 import 'package:cropify/screens/auth/signup.dart';
-import 'package:cropify/screens/common/appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -14,50 +13,94 @@ class Login extends GetWidget<AuthController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: const CropifyAppBar(visible: false),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextFormField(
-                decoration: const InputDecoration(hintText: "Email"),
-                controller: emailController,
+      body: Column(
+        children: [
+          Container(
+            alignment: Alignment.center,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/vegies.jpg"),
+                fit: BoxFit.cover,
               ),
-              const SizedBox(
-                height: 40,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: "Password",
-                ),
-                controller: passwordController,
-                obscureText: true,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                child: const Text("Log In"),
-                onPressed: () {
-                  controller.login(
-                      emailController.text, passwordController.text);
-                },
-              ),
-              TextButton(
-                child: const Text(
-                  "Sign Up",
-                  style: TextStyle(color: Color.fromARGB(255, 2, 70, 2)),
-                ),
-                onPressed: () {
-                  Get.to(() => SignUp());
-                },
-              )
-            ],
+            ),
+            height: 250,
+            width: double.infinity,
+            child: const Text(
+              'Cropify',
+              style: TextStyle(
+                  color: Color.fromARGB(255, 255, 255, 255),
+                  fontWeight: FontWeight.w100,
+                  fontSize: 70,
+                  fontFamily: "Lobster",
+                  shadows: [
+                    Shadow(
+                        // bottomLeft
+                        offset: Offset(-1.5, -1.5),
+                        color: Color.fromARGB(255, 2, 70, 2)),
+                    Shadow(
+                        // bottomRight
+                        offset: Offset(1.5, -1.5),
+                        color: Color.fromARGB(255, 2, 70, 2)),
+                    Shadow(
+                        // topRight
+                        offset: Offset(1.5, 1.5),
+                        color: Color.fromARGB(255, 2, 70, 2)),
+                    Shadow(
+                        // topLeft
+                        offset: Offset(-1.5, 1.5),
+                        color: Color.fromARGB(255, 2, 70, 2)),
+                  ]),
+            ),
           ),
-        ),
+          Expanded(
+            child: Center(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      TextFormField(
+                        decoration: const InputDecoration(hintText: "Email"),
+                        controller: emailController,
+                      ),
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: "Password",
+                        ),
+                        controller: passwordController,
+                        obscureText: true,
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      ElevatedButton(
+                        child: const Text("Log In"),
+                        onPressed: () {
+                          controller.login(
+                              emailController.text, passwordController.text);
+                        },
+                      ),
+                      TextButton(
+                        child: const Text(
+                          "Sign Up",
+                          style:
+                              TextStyle(color: Color.fromARGB(255, 2, 70, 2)),
+                        ),
+                        onPressed: () {
+                          Get.to(() => SignUp());
+                        },
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/screens/auth/signup.dart
+++ lib/screens/auth/signup.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/state_manager.dart';
 
 import '../../controllers/auth_controller.dart';
-import '../common/appbar.dart';
 
 class SignUp extends GetWidget<AuthController> {
   final TextEditingController emailController = TextEditingController();
@@ -13,39 +12,85 @@ class SignUp extends GetWidget<AuthController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: const CropifyAppBar(visible: false),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextFormField(
-                decoration: const InputDecoration(hintText: "Email"),
-                controller: emailController,
+      appBar: AppBar(backgroundColor: const Color.fromARGB(255, 2, 70, 2)),
+      body: Column(
+        children: [
+          Container(
+            alignment: Alignment.center,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/vegies.jpg"),
+                fit: BoxFit.cover,
               ),
-              const SizedBox(
-                height: 40,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(hintText: "Password"),
-                obscureText: true,
-                controller: passwordController,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                child: const Text("Sign Up"),
-                onPressed: () {
-                  controller.createUser(
-                      emailController.text, passwordController.text);
-                },
-              ),
-            ],
+            ),
+            height: 250,
+            width: double.infinity,
+            child: const Text(
+              'Cropify',
+              style: TextStyle(
+                  color: Color.fromARGB(255, 255, 255, 255),
+                  fontWeight: FontWeight.w100,
+                  fontSize: 70,
+                  fontFamily: "Lobster",
+                  shadows: [
+                    Shadow(
+                        // bottomLeft
+                        offset: Offset(-1.5, -1.5),
+                        color: Color.fromARGB(255, 2, 70, 2)),
+                    Shadow(
+                        // bottomRight
+                        offset: Offset(1.5, -1.5),
+                        color: Color.fromARGB(255, 2, 70, 2)),
+                    Shadow(
+                        // topRight
+                        offset: Offset(1.5, 1.5),
+                        color: Color.fromARGB(255, 2, 70, 2)),
+                    Shadow(
+                        // topLeft
+                        offset: Offset(-1.5, 1.5),
+                        color: Color.fromARGB(255, 2, 70, 2)),
+                  ]),
+            ),
           ),
-        ),
+          Expanded(
+            child: Center(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      TextFormField(
+                        decoration: const InputDecoration(hintText: "Email"),
+                        controller: emailController,
+                      ),
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: "Password",
+                        ),
+                        controller: passwordController,
+                        obscureText: true,
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      ElevatedButton(
+                        child: const Text("Sign Up"),
+                        onPressed: () {
+                          controller.createUser(
+                              emailController.text, passwordController.text);
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
