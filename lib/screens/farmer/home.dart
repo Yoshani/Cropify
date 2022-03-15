9e7480aaad04f4500d795f159b844fb0b39dbaa6
Yoshani <yoshani.malinka@gmail.com>
2022-03-15 12:35:47 +0530

    added ads display carousal

--- lib/screens/farmer/home.dart
+++ lib/screens/farmer/home.dart
@@ -1,4 +1,6 @@
+import 'package:cropify/utils/carousal_items.dart';
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class FarmerHome extends StatelessWidget {
   const FarmerHome({Key? key}) : super(key: key);
@@ -12,7 +14,7 @@ class FarmerHome extends StatelessWidget {
             alignment: Alignment.center,
             decoration: const BoxDecoration(
               image: DecorationImage(
-                image: AssetImage("assets/vegies-home.jpg"),
+                image: AssetImage("assets/vegie-home.jpg"),
                 fit: BoxFit.cover,
               ),
             ),
@@ -38,7 +40,119 @@ class FarmerHome extends StatelessWidget {
                         fontSize: 20,
                       ),
                     ),
-                  )
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Expanded(
+                    child: CarouselSlider(
+                      options: CarouselOptions(
+                        enableInfiniteScroll: true,
+                        scrollDirection: Axis.vertical,
+                        autoPlay: true,
+                        autoPlayInterval: const Duration(seconds: 15),
+                        autoPlayCurve: Curves.decelerate,
+                        viewportFraction: 1.0,
+                      ),
+                      items: adCarousal.map((i) {
+                        return Builder(
+                          builder: (BuildContext context) {
+                            return Container(
+                                width: MediaQuery.of(context).size.width,
+                                margin:
+                                    const EdgeInsets.symmetric(horizontal: 5.0),
+                                decoration:
+                                    const BoxDecoration(color: Colors.amber),
+                                child: Card(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Row(
+                                      children: [
+                                        Expanded(
+                                          child: Container(
+                                            alignment: Alignment.center,
+                                            decoration: BoxDecoration(
+                                              image: DecorationImage(
+                                                image: i.image,
+                                                fit: BoxFit.cover,
+                                              ),
+                                            ),
+                                            height: 250,
+                                            width: double.infinity,
+                                          ),
+                                        ),
+                                        const SizedBox(
+                                          width: 10,
+                                        ),
+                                        Expanded(
+                                            child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.end,
+                                              children: const [
+                                                Text(
+                                                  "Ad",
+                                                  style: TextStyle(
+                                                    fontSize: 10,
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                            const SizedBox(
+                                              height: 10,
+                                            ),
+                                            Expanded(
+                                              child: Column(
+                                                children: [
+                                                  Text(
+                                                    i.title,
+                                                    style: const TextStyle(
+                                                      fontSize: 18,
+                                                      fontWeight:
+                                                          FontWeight.w500,
+                                                    ),
+                                                  ),
+                                                  const SizedBox(
+                                                    height: 15,
+                                                  ),
+                                                  Expanded(
+                                                    child: Text(
+                                                      i.description,
+                                                      overflow:
+                                                          TextOverflow.fade,
+                                                      maxLines: null,
+                                                      style: const TextStyle(
+                                                          fontSize: 12,
+                                                          fontWeight:
+                                                              FontWeight.w300,
+                                                          color: Color.fromARGB(
+                                                              255, 63, 62, 62)),
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
+                                            ),
+                                            ElevatedButton(
+                                              child: const Text("Read more..."),
+                                              onPressed: () {},
+                                            ),
+                                          ],
+                                        ))
+                                      ],
+                                    ),
+                                  ),
+                                ));
+                          },
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
                 ],
               ),
             ),
