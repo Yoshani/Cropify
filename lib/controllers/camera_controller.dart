46cf17dc2bbf7fd2ce53202677b13a32e551f100
Yoshani <yoshani.malinka@gmail.com>
2022-03-26 12:38:27 +0530

    removed file picker

--- lib/controllers/camera_controller.dart
+++ lib/controllers/camera_controller.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:cropify/models/media.dart';
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
